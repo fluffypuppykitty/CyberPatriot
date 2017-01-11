d02a25139db3604365e915c5b347be1c094847d9
conner-m <conner-m@users.noreply.github.com>
2017-01-11 15:13:28 -0600

    Update AutoConfig.bat

--- Windows/AutoConfig.bat
+++ Windows/AutoConfig.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Setting up password policies
 net accounts /minpwlen:7 /maxpwage:30 /minpwage:10 /uniquepw:1 /forcelogoff:no /lockoutduration:30 /lockoutthreshold:7 
-echo Password Policies:
+echo Password Policies Set:
 echo Length: 7
 echo Max Age: 30 Days
 echo Min Age: 10 Days
@@ -10,4 +10,6 @@ echo Force Logoff: no
 echo Lockout Duration: 30 Minutes
 echo Lockout Threshold: 7
 secedit.exe /export /cfg C:\secconfig.cfg
-REM secedit.exe /configure /db %windir%\securitynew.sdb /cfg C:\secconfig.cfg /areas SECURITYPOLICY
+echo Use this command to update edited file: secedit.exe /configure /db %windir%\securitynew.sdb /cfg C:\secconfig.cfg /areas SECURITYPOLICY
+echo Setting up auditing
+auditpol /set /category:"Account Logon" /Failure:Enable
