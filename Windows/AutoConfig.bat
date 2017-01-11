f9d5d0f2b5df4d1d71ad1d96f24b5fff3dac5db9
conner-m <conner-m@users.noreply.github.com>
2017-01-11 15:00:56 -0600

    Update AutoConfig.bat

--- Windows/AutoConfig.bat
+++ Windows/AutoConfig.bat
@@ -3,8 +3,11 @@ echo Setting up password policies
 net accounts /minpwlen:7 /maxpwage:30 /minpwage:10 /uniquepw:1 /forcelogoff:no /lockoutduration:30 /lockoutthreshold:7 
 echo Password Policies:
 echo Length: 7
-echo Max Age: 30
+echo Max Age: 30 Days
+echo Min Age: 10 Days
+echo Times Password can be resused: 1
+echo Force Logoff: no
+echo Lockout Duration: 30 Minutes
+echo Lockout Threshold: 7
 secedit.exe /export /cfg C:\secconfig.cfg
 REM secedit.exe /configure /db %windir%\securitynew.sdb /cfg C:\secconfig.cfg /areas SECURITYPOLICY
-
-auditpol
